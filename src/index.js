63ecde3de094843c30b4c3958a40c8c02ab1a604
LuisAlexisSalazar <kirito_SWO@outlook.com>
2022-10-25 09:59:04 -0500

    Clon de repositorio react

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+
+/* eslint-disable import/prefer-default-export */
+import AlignmentViewer from './components/AlignmentViewer';
+import AlignmentChart from './components/AlignmentChart';
+
+export {
+    AlignmentViewer,
+    AlignmentChart
+};
+
+
